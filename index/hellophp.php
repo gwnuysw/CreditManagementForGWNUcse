71d70405b367f17b9374514b6114669c02f887f4
석원 <seok-won@seog-won-ui-iMac.local>
2018-11-19 15:29:35 +0900

    mysql password problem fix

--- index/hellophp.php
+++ index/hellophp.php
@@ -1,4 +1,20 @@
 <?php
-  header("Location:http://localhost/admin.html", true, 301);
-  exit();
+  $id = "";
+  $pw = 0;
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = test_input($_POST["id"]);
+    $pw = test_input($_POST["password"]);
+  }
+
+  function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+  if($id == "admin" && $pw == 1234){
+    header("Location:http://localhost/admin.html", true, 301);
+    exit();
+  }
 ?>
