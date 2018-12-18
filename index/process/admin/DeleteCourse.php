09c4507b6e5e4850abb7b1728f716ee198846902
seokwon <swlee1014@gmail.com>
2018-12-18 22:12:33 +0900

    과목 삭제 수정

--- index/process/admin/DeleteCourse.php
+++ index/process/admin/DeleteCourse.php
@@ -1,13 +1,17 @@
 <?php
 	$coursecode = safety_param($_POST["coursecode"]);
-	$sql = "SELECT * FROM `course` WHERE `coursecode` = '{$coursecode}'";
+	$period = safety_param($_POST["period"]);
+
+	$sql = "SELECT * FROM `course` WHERE `coursecode` = '".$coursecode."' and `period` = '".$period."'";
+
 	if ($db_conn->query($sql)->num_rows) {
-		$sql = "DELETE FROM `course` WHERE `coursecode` = '{$coursecode}'";
+		$sql = "DELETE FROM `course` WHERE `coursecode` = '".$coursecode."' and `period` = '".$period."'";
 		if ($db_conn->query($sql)) {
 			alert("정상적으로 삭제되었습니다.","/admin");
 		} else {
 			alert("에러가 발생하였습니다.\r\b".$db_conn->error,"/admin");
 		}
 	} else {
-		alert("존재하지 않는 과목코드 입니다.","/admin");
+		echo $period;
+		//alert("존재하지 않는 과목코드 입니다. ","/admin");
 	}
